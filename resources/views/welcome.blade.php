0bd57763d8d63a02a418b3f5989895856c8a90fb
hamamizadah <96341466+hamamizadah@users.noreply.github.com>
2022-02-23 00:16:59 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
        <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Inda Khoirun Nisak</h1></div>
+            <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Muhammad Hamamiy Zadah</h1></div>
         </div>
     </body>
     </body>
